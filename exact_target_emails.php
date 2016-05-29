57501256e75b1d79addfc63d0b87dd8560e2f3e2
Vik Ewoods <vik.ewoods@gmail.com>
2016-05-29 14:46:51 +0100

    Initial commit

---
+++ exact_target_emails.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Exact Target Email Form
+ * Plugin URI: http://www.riseproject.com/
+ * Version: 1.0.1
+ * Author: Vik Ewoods
+ * Description: Integration of SalesForce (Exact Target) Email subscription form
+ **/
+
+
+
+
+
+?>
